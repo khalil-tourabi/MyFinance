32d4a4dc3af43b1f72c01ba9d67a95c726bde934
khalil-tourabi <ktourabi@gmail.com>
2024-07-09 13:57:58 +0100

    added frontend working envirenment

--- Backend/app.js
+++ Backend/app.js
@@ -1,5 +1,7 @@
 import express from 'express'
 const app = express();
+import dotenv from 'dotenv';
+dotenv.config();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -9,12 +11,12 @@ import authRoute from './src/routes/authRoutes.js';
 app.use(authRoute);
 
 app.all('*', (req, res, next) => {
-    res.status(400).json({success: false, msg: 'wrong url path'})
+    res.status(400).json({ success: false, msg: 'wrong url path' })
     console.log(`${req.originalUrl} doesnt exist`)
     next()
 })
 
-const port=process.env.APP_PORT || 8000
-app.listen(port,()=>{
- console.log(`Server is running on PORT ${port}....`)
+const port = process.env.APP_PORT || 8000
+app.listen(port, () => {
+    console.log(`Server is running on PORT ${port}....`)
 })
