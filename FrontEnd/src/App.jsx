33189102b2a6f38c6113894c3eae8c3e5184fbf5
khalil-tourabi <ktourabi@gmail.com>
2024-07-10 09:52:12 +0100

    created login and signup pages

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,9 +1,14 @@
 import Accueil from "./components/Accueil/Accueil";
+import Login from "./components/Login/Login";
+import NavbarUnlogged from "./components/navbars/Navbar-unlogged";
+import Register from "./components/Register/Register";
 
 function App() {
   return (
     <>
-      <Accueil />
+      <Register />
+      <NavbarUnlogged />
+      <Login />
     </>
   );
 }
